8c83212cbfbb65687564486d9d6296bdd3bb6671
Rafi Hadiyasa <rafihadiyasa32@gmail.com>
2024-09-30 20:34:41 +0700

    Validation with ZOD

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,6 +1,11 @@
 "use server";
 
+import z from "zod";
+
 export const saveContact = async (formData: FormData) => {
   const data = Object.fromEntries(formData.entries());
   console.log(data);
+
+  // Validasi agar data yang masuk ke database adalah data yang valid
+  // Validasi menggunakan zod
 };
